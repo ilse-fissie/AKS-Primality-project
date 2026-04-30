6bfc5fbbeb56b144bad0d7ba057ffcb50477a2ec
ilse <i.s.knevel@umail.leidenuniv.nl>
2026-04-30 13:16:14 +0200

    End of lecture 30/04

--- FMiMProject/Basic.lean
+++ FMiMProject/Basic.lean
@@ -7,12 +7,13 @@ We start with an example theorem we need to prove. For this we
 will also need to install the right packages etc.
 -/
 
+def hello := "world"
 
 theorem Theorem1 {R : Type u_1} [CommSemiring R] (n : ℕ)
 (x :R) [CharP R n] : n.Prime ↔  (x+1)^n = x^n+1 := by
   constructor
   · intro h
-    rw [← one_zpow]
-    apply add_pow_char x 1
-    sorry
+    have name : Fact (Nat.Prime n) := {out := h}
+    rw [add_pow_char x 1]
+    simp
   · sorry

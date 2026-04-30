c56de8d4b6fd157dd008c201f1631da312218f6d
ilse <i.s.knevel@umail.leidenuniv.nl>
2026-04-30 12:34:03 +0200

    Start with example proof

--- FMiMProject/Basic.lean
+++ FMiMProject/Basic.lean
@@ -1,1 +1,12 @@
-def hello := "food"
+import Mathlib.Tactic
+import Mathlib.RingTheory.SimpleRing.Basic
+
+-- Mathlib.Algebra
+/-
+We start with an example theorem we need to prove. For this we
+will also need to install the right packages etc.
+-/
+
+
+theorem Theorem1 {R : Type u_1} [CommSemiring R] (n : ℕ)
+(x :R) [CharP R n] : n.Prime ↔  (x+1)^n = x^n+1 := by sorry

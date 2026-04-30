46a84691f548b416ff9dcd17d9813024546ce136
Hayven Hagedoorn <n.j.h.hagedoorn@umail.leidenuniv.nl>
2026-04-30 12:50:54 +0200

    delete the test

--- FMiMProject/Basic.lean
+++ FMiMProject/Basic.lean
@@ -7,7 +7,6 @@ We start with an example theorem we need to prove. For this we
 will also need to install the right packages etc.
 -/
 
-def hello := 'world'
 
 theorem Theorem1 {R : Type u_1} [CommSemiring R] (n : ℕ)
 (x :R) [CharP R n] : n.Prime ↔  (x+1)^n = x^n+1 := by sorry
